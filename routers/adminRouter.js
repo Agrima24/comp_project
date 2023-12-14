6d8ba79aa56da07af7e95ff5eb8196f8afb3f809
Agrima24 <agrimaagarwal722@gmail.com>
2023-12-14 09:16:13 -0500

    create getID route

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -6,5 +6,6 @@ const admin = require('../controllers/adminController');
 router.post('/assign-table',admin.createAssignment);
 router.get('/getAssign-user/:user_id',admin.getAssignmentsForUser);
 router.post('/acknowledge/:user_id/:candidate_id',admin.acknowledgeAssignment);
+router.get('/getID',admin.getId)
 
 module.exports = router

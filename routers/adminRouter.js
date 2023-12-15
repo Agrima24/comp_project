678b390842d106a3a1ca85f3f88bc794d176d6a9
Agrima24 <agrimaagarwal722@gmail.com>
2023-12-15 09:42:02 -0500

    create getNotification router

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -6,6 +6,7 @@ const admin = require('../controllers/adminController');
 router.post('/assign-table',admin.createAssignment);
 router.get('/getAssign-user/:user_id',admin.getAssignmentsForUser);
 router.post('/acknowledge/:user_id/:candidate_id',admin.acknowledgeAssignment);
+router.get('/getNotification',admin.getNotify);
 router.get('/getID',admin.getId)
 
 module.exports = router
